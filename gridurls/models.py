3e5a733441c50d1fb045d5e5519890fa2c3a7ca4
Nick 'zoni' Groenen <zoni@zoni.nl>
2010-04-18 23:30:57 +0200

    hashlib.sha256().hexdigest() generates a 65 character string, not 256. Whoops. Model adjusted

--- gridurls/models.py
+++ gridurls/models.py
@@ -4,6 +4,6 @@ class Url(models.Model):
 	name = models.CharField(max_length=255, unique=True)
 	inworld_url = models.CharField(max_length=255)
 	password_salt = models.CharField(max_length=32)
-	password_hash = models.CharField(max_length=256)
+	password_hash = models.CharField(max_length=65)
 	last_modified = models.DateTimeField(auto_now=True, auto_now_add=True)
 
