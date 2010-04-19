66613cb1c5da0dcc08d8960bc56d93e43613f306
Nick 'zoni' Groenen <zoni@zoni.nl>
2010-04-19 18:00:47 +0200

    Change registration process
    /generate becomes /register, /register becomes /update
    Trying to update a non-existing name no longer transparently creates
    that url, but returns a 404 instead.
    Registration allows registrating custom name and password via POST
    variables

--- gridurls/views.py
+++ gridurls/views.py
@@ -1,6 +1,6 @@
 from gridurls.models import Url
 from django.http import HttpResponse, HttpResponseRedirect
-import hashlib, string, random
+import hashlib, string, random, re
 from uuid import uuid4
 import settings
 
@@ -8,8 +8,8 @@ def index(request):
 	# Simply redirect to project homepage until a proper index page is written
 	return HttpResponseRedirect("http://zoni.nl/django-gridurl")
 
-def register(request, name):
-	# Register can only be used with POST request method
+def update(request, name):
+	# Update can only be used with POST request method
 	if request.method != "POST":
 		response = HttpResponse(mimetype='text/plain', status=405)
 		response['Allow'] = "POST"
@@ -18,37 +18,32 @@ def register(request, name):
 
 	response = HttpResponse(mimetype='text/plain')
 
+	# Assume blank password if omitted
 	if 'password' in request.POST.keys():
 		password = request.POST['password']
 	else:
 		password = ''
 
-	if 'url' in request.POST.keys():
-		url = request.POST['url']
-	else:
-		url = ''
-
-	# Try and get <name> from the database. If <name> does not exist, create it
-	# with the values passed in the request. This makes registering custom name+password
-	# combinations possible
 	try:
 		r = Url.objects.get(name=name)
 	except Url.DoesNotExist:
-		# Generate random password salt consisting of salt_length (Configured in settings.py)
-		# characters in the range A-Z,a-z,0-9
-		salt = "".join([random.choice(string.letters+string.digits) for x in range(settings.SALT_LENGTH)])
-		hash = hashlib.sha256(salt + password).hexdigest()
-
-		r = Url(name=name, password_salt=salt, password_hash=hash, inworld_url=url)
-		r.save()
-		response.write("OK")
+		response['status'] = 404
+		response.write("Requested name not found")
 		return response
 
 	hash = hashlib.sha256(r.password_salt + password).hexdigest()
 	if hash == r.password_hash:
-		response.write("OK")
-		r.inworld_url = url
+		if 'url' in request.POST.keys():
+			r.inworld_url = request.POST['url']
+		if 'newpassword' in request.POST.keys():
+			if request.POST['newpassword'] == "__GENERATE__":
+				newpassword = "".join([random.choice(string.letters+string.digits) for x in range(settings.PASSWORD_LENGTH)])
+			else:
+				newpassword = request.POST['newpassword']
+			r.password = hashlib.sha256(r.salt + newpassword).hexdigest()
+
 		r.save()
+		response.write("OK")
 	else:
 		response['status'] = 401
 		response.write("Password incorrect")
@@ -57,16 +52,16 @@ def register(request, name):
 
 def get(request, name):
 	response = HttpResponse(mimetype='text/plain')
-	
+
 	try:
 		r = Url.objects.get(name=name)
 	except Url.DoesNotExist:
 		response['status'] = 404
-		response.write("{0} not found".format(name))
+		response.write("Requested name not found")
 		return response
-	
+
 	if r.inworld_url == "":
-		return HttpResponse("{0} currently disabled".format(name), mimetype='text/plain', status=503)
+		return HttpResponse("{0} is currently disabled".format(name), mimetype='text/plain', status=503)
 	else:
 		response.write(r.inworld_url)
 		return response
@@ -75,26 +70,50 @@ def go(request, name):
 	try:
 		r = Url.objects.get(name=name)
 	except Url.DoesNotExist:
-		return HttpResponse("{0} not found".format(name), mimetype='text/plain', status=404)
-	
+		return HttpResponse("Requested name not found", mimetype='text/plain', status=404)
+
 	if r.inworld_url == "":
-		return HttpResponse("{0} currently disabled".format(name), mimetype='text/plain', status=503)
+		return HttpResponse("{0} is currently disabled".format(name), mimetype='text/plain', status=503)
 	else:
 		return HttpResponseRedirect(r.inworld_url)
 
-def generate(request):
-	name = str(uuid4())
-	# Generate random password salt consisting of salt_length (Configured in settings.py)
-	# characters in the range A-Z,a-z,0-9
-	salt = "".join([random.choice(string.letters+string.digits) for x in range(settings.SALT_LENGTH)])
-	# Same for the actual password
-	password = "".join([random.choice(string.letters+string.digits) for x in range(settings.PASSWORD_LENGTH)])
-	hash = hashlib.sha256(salt + password).hexdigest()
-
-	r = Url(name=name, password_salt=salt, password_hash=hash)
-	r.save()
-	
+def register(request):
 	response = HttpResponse(mimetype='text/plain')
-	response.write("OK;{0};{1}".format(name,password))
+
+	# Get name from POST values if present, generate unique ID used as name otherwise
+	if request.method == "POST" and 'name' in request.POST.keys():
+		name = request.POST['name']
+	else:
+		name = str(uuid4())
+	# Get password from POST values if present, generate random one otherwise
+	if request.method == "POST" and 'password' in request.POST.keys():
+		password = request.POST['password']
+	else:
+		password = "".join([random.choice(string.letters+string.digits) for x in range(settings.PASSWORD_LENGTH)])
+	# And for the url, blank if not passed along in the request
+	if request.method == "POST" and 'url' in request.POST.keys():
+		url = request.POST['url']
+	else
+		url = ""
+
+	if Url.objects.filter(name=name).count() > 0:
+		response['status'] = 403
+		response.write("{0} is already registered".format(name))
+	# Allow only a-z, A-Z ,0-9, _ and - characters in name
+	else if: re.search(r'[^\w-]+', name) != None
+		response['status'] = 403
+		response.write("{0} contains illegal characters. [Only a-zA-Z0-9-_] allowed".format(name))
+	else:
+		# Generate random password salt consisting of salt_length (Configured in settings.py)
+		# characters in the range A-Z,a-z,0-9
+		salt = "".join([random.choice(string.letters+string.digits) for x in range(settings.SALT_LENGTH)])
+		# Compute the hash of the salt+password
+		hash = hashlib.sha256(salt + password).hexdigest()
+
+		r = Url(name=name, password_salt=salt, password_hash=hash, url=url)
+		r.save()
+		
+		response.write("OK;{0};{1}".format(name,password))
+
 	return response
 
